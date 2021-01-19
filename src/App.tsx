cef040f6f22d7bc04a4ac29236b3e2089330e3d2
Keith Broughton <keithbro256@gmail.com>
2021-01-19 11:25:22 +1100

    fp-ts FTW

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,25 @@ import styled from "styled-components";
 import Grid from "./components/Grid";
 import { GameContext } from "./GameContext";
 import { Player, Spaces, Mark } from "./types";
-import { left, right, Either, isRight } from "fp-ts/lib/Either";
+import * as E from "fp-ts/lib/Either";
+import * as O from "fp-ts/lib/Option";
 
 const Container = styled.div`
   align-items: center;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+`;
+
+const Info = styled.div`
+  margin: 10px;
 `;
 
 class SpaceAlreadyOccupiedError extends Error {}
 
-type MarkSpaceResult = {
+type TakeTurnResult = {
   spaces: Spaces;
-  nextPlayer: Player;
+  nextPlayer?: Player;
 };
 
 const playerOne: Player = { mark: Mark.X };
@@ -25,16 +31,41 @@ const markSpace = (
   index: number,
   spaces: Spaces,
   currentPlayer: Player
-): Either<Error, MarkSpaceResult> => {
+): E.Either<SpaceAlreadyOccupiedError, Spaces> => {
   const space = spaces[index];
-  if (space) return left(new SpaceAlreadyOccupiedError());
+  if (space)
+    return E.left(
+      new SpaceAlreadyOccupiedError(`Space ${index} already occupied!`)
+    );
+
+  spaces[index] = currentPlayer.mark;
+
+  return E.right(spaces);
+};
+
+const checkForWinner = (spaces: Spaces): O.Option<Player> => {
+  console.log({ spaces });
+  return O.none;
+};
+
+const takeTurn = (
+  index: number,
+  spaces: Spaces,
+  currentPlayer: Player
+): E.Either<Error, TakeTurnResult> => {
+  const result = markSpace(index, spaces, currentPlayer);
+
+  if (E.isLeft(result)) return E.left(result.left);
+
+  const { right: newSpaces } = result;
+
+  const winner = checkForWinner(newSpaces);
+  if (O.isSome(winner)) return E.right({ spaces: newSpaces });
 
   const nextPlayer =
     currentPlayer.mark === playerOne.mark ? playerTwo : playerOne;
 
-  spaces[index] = currentPlayer.mark;
-
-  return right({ spaces, nextPlayer });
+  return E.right({ spaces: result.right, nextPlayer });
 };
 
 function App() {
@@ -45,18 +76,23 @@ function App() {
   const [currentPlayer, setCurrentPlayer] = useState<Player>(playerOne);
 
   const setSpace = (index: number) => {
-    const result = markSpace(index, spaces, currentPlayer);
+    const result = takeTurn(index, spaces, currentPlayer);
 
-    if (isRight(result)) {
-      setSpaces(result.right.spaces);
-      setCurrentPlayer(result.right.nextPlayer);
+    if (E.isLeft(result)) {
+      console.warn(result.left.message);
+      return;
     }
+
+    setSpaces(result.right.spaces);
+
+    if (result.right.nextPlayer) setCurrentPlayer(result.right.nextPlayer);
   };
 
   return (
     <Container>
       <GameContext.Provider value={{ spaces, setSpace }}>
         <Grid />
+        <Info>Player Turn: {currentPlayer.mark}</Info>
       </GameContext.Provider>
     </Container>
   );
